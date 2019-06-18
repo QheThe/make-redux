07cfa2f5af2b2e916256def4a2b4fdd4a8d6d7a7
2360527568 <2360527568@qq.com>
2019-06-18 08:27:41 +0800

    dispatch 调用 & 重新渲染页面

--- src/index.js
+++ src/index.js
@@ -19,42 +19,43 @@ const appState = {
     content: {
         text: 'React.js 小书内容',
         color: 'blue'
-    } 
+    }
 }
 
-function dispatch (action) {
-	switch(action.type) {
-		case 'UPDATE_TITLE_TEXT':
-			appState.title.text = action.text
-			break
-		case 'UPDATE_TITLE_COLOR':
-			appState.title.color = action.color
-			break
-		default:
-			break
+function dispatch(action) {
+    switch (action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            appState.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            appState.title.color = action.color
+            break
+        default:
+            break
 
-	}
+    }
 }
 
-function renderApp (appState) {
+function renderApp(appState) {
     renderTitle(appState.title)
     renderContent(appState.content)
 }
 
-function renderTitle (title) {
+function renderTitle(title) {
     const titleDOM = document.getElementById('title')
     titleDOM.innerHTML = title.text
     titleDOM.style.color = title.color
 }
 
-function renderContent (content) {
+function renderContent(content) {
     const contentDOM = document.getElementById('content')
     contentDOM.innerHTML = content.text
     contentDOM.style.color = content.color
 }
- 
 
-renderApp(appState)
+renderApp(appState) // 首次渲染页面
 
-// 更新标题文字
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '更新标题文字'})
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
+
+renderApp(appState) // 把新的数据渲染到页面上
